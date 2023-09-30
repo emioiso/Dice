a5e901003af625f40bb324002dbd22e60b997fca
emioiso <jijjjiijiijijiji@gmail.com>
2023-09-30 17:24:44 +0900

    .gitignore追加

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomeNumner = 1
+    @State private var timer: Timer?
+    @State private var isRolling = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: "die.face.\(randomeNumner)")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.main.bounds.width/2)
+                .padding()
+            Spacer()
+            Button {
+                playDice()
+                
+            } label: {
+                Text("サイコロをふる")
+                    .padding()
+                    .background(Color.orange)
+                    .foregroundColor(.blue)
+                    .cornerRadius(10)
+            }
+            .disabled(isRolling)
+            Spacer()
         }
         .padding()
+    }
+    private func playDice(){
+        print("ボタンが押されたよ")
+        isRolling = true
+        randomeNumner = Int.random(in: 1...6)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            randomeNumner = Int.random(in: 1...6)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+            timer?.invalidate()
+            timer = nil
+            isRolling = false
+        }
     }
 }
 
